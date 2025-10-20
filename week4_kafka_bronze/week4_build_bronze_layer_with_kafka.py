20cb7111b37f94c45b8ad7e1cc021b8486222d01
sjcswank <sjcswank@gmail.com>
2025-10-20 16:54:25 -0500

    completed week4_kafka_bronze

--- week4_kafka_bronze/week4_build_bronze_layer_with_kafka.py
+++ week4_kafka_bronze/week4_build_bronze_layer_with_kafka.py
@@ -1,6 +1,6 @@
 import os
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, current_timestamp
+from pyspark.sql.functions import col, current_timestamp, split
 
 # Load environment variables.
 from dotenv import load_dotenv
@@ -18,6 +18,7 @@ username = os.environ.get("HWE_USERNAME")
 password = os.environ.get("HWE_PASSWORD")
 aws_access_key_id = os.environ.get("AWS_ACCESS_KEY_ID")
 aws_secret_access_key = os.environ.get("AWS_SECRET_ACCESS_KEY")
+aws_handle = os.environ.get("AWS_HANDLE")
 kafka_topic = "reviews"
 
 # Create a SparkSession
@@ -47,10 +48,43 @@ df = spark \
     .option("kafka.security.protocol", "SASL_SSL") \
     .option("kafka.sasl.mechanism", "SCRAM-SHA-512") \
     .option("kafka.sasl.jaas.config", getScramAuthString(username, password)) \
-    .load()
+    .load() \
+    .select(col('value').cast('string').alias('review')) \
+    .select(split('review', '\t').alias("review")) \
+    .selectExpr(
+      "review[0] as marketplace",
+      "review[1] as customer_id",
+      "review[2] as review_id",
+      "review[3] as product_id",
+      "review[4] as product_parent",
+      "review[5] as product_title",
+      "review[6] as product_category",
+      "review[7] as star_rating",
+      "review[8] as helpful_votes",
+      "review[9] as total_votes",
+      "review[10] as vine",
+      "review[11] as verified_purchase",
+      "review[12] as review_headline",
+      "review[13] as review_body",
+      "review[14] as purchase_date"
+    ) \
+    .withColumn("review_timestamp", current_timestamp())
+
+# columns = ["marketplace", "customer_id"]
+# df = df.select([col('review').getItem(i).alias(columns[i]) for i in range(0, 14)])
 
 # Process the received data
-query = None
+df.printSchema()
+
+save_location = f"s3a://hwe-fall-2025/{aws_handle}/bronze/reviews"
+
+query = df \
+  .writeStream \
+  .outputMode("append") \
+  .format("parquet") \
+  .option("path", save_location) \
+  .option("checkpointLocation", "/tmp/kafka-checkpoint") \
+  .start()
 
 # Wait for the streaming query to finish
 query.awaitTermination()
